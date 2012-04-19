135de484792db96af98e47c405d66da36be3e454
Kenneth Powers <mail@kenpowers.net>
2012-04-19 18:45:18 -0400

    Added error notification for "data:" URIs.

--- context.js
+++ context.js
@@ -1,6 +1,11 @@
 function search(info, tab) {
-	var url = 'http://karmadecay.com/' + encodeURIComponent(info.srcUrl);
-	chrome.tabs.create({url: url, selected: true });
+	var src = info.srcUrl;
+	if (src.indexOf('data:') == 0) {
+		webkitNotifications.createNotification('', 'Error!', '"data:" URIs are not currently supported.').show();
+		return;
+	}
+	var reqUrl = 'http://karmadecay.com/' + encodeURIComponent(src);
+	chrome.tabs.create({url: reqUrl, selected: true });
 }
 
 chrome.contextMenus.create({'title': 'Search for this image on Karma Decay', 'contexts': ['image'], 'onclick': search});
