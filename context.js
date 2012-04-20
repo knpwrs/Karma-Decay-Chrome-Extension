d276d05dd1a42953e2a840894d1ba56cb5701425
Kenneth Powers <mail@kenpowers.net>
2012-04-19 23:19:17 -0400

    Added icon to error notification.

--- context.js
+++ context.js
@@ -1,7 +1,7 @@
 function search(info, tab) {
 	var src = info.srcUrl;
 	if (src.indexOf('data:') == 0) {
-		webkitNotifications.createNotification('', 'Error!', '"data:" URIs are not currently supported.').show();
+		webkitNotifications.createNotification('img/icon-48.png', 'Error!', '"data:" URIs are not currently supported.').show();
 		return;
 	}
 	var reqUrl = 'http://karmadecay.com/' + encodeURIComponent(src);
